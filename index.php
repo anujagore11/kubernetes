c761c906229f47c525faf69d60ff4c3f752f5b4d
anujagore11 <anujgore11@gmail.com>
2018-11-14 18:30:12 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1 align='centre'>My first PHP page using Docker image and FLUX</h1>
+
+<?php
+echo "Hello World! Hello Kubernetes! It Works !!!";
+?> 
+
+</body>
+</html>
